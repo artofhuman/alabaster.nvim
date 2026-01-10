7c1fc44788087855d8282f469b42b099c5b3864b
Semyon Pupkov <mail@semyonpupkov.com>
2026-01-10 13:12:04 +0500

    Fix issue in scope_resolution

--- after/queries/ruby/highlights.scm
+++ after/queries/ruby/highlights.scm
@@ -5,6 +5,10 @@
 (method
   name: (identifier) @AlabasterDefinition)
 
+;; singleton methods (def self.method)
+(singleton_method
+  name: (identifier) @AlabasterDefinition)
+
 ;; simple classes
 (class
   name: (constant) @AlabasterDefinition)
@@ -12,6 +16,12 @@
 ;; modules
 (module
   name: (constant) @AlabasterDefinition)
+
+;; all constants in scope_resolution chains (handles any nesting depth)
+(scope_resolution
+  name: (constant) @AlabasterDefinition)
+(scope_resolution
+  scope: (constant) @AlabasterDefinition)
 
 ((simple_symbol) @AlabasterConstant)
 ((hash_key_symbol) @AlabasterConstant)
